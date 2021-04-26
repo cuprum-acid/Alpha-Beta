dc21c479c6de508ca3229398221baeef21ff6be6
cuprum-acid <jony.zh@yandex.ru>
2021-04-26 20:38:38 +0300

    alphabet is done

---
+++ app/src/main/java/com/example/alphabeta/MainMenu.kt
@@ -0,0 +1,13 @@
+package com.example.alphabeta
+
+import android.os.Bundle
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+
+
+class MainMenu : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main_menu)
+    }
+}
